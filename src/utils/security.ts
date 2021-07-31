cd2cd4aac852de018ecc81192912a1c101cbac7f
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-07-31 12:20:45 -0300

    alter name variables and add function docstring

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,18 +1,56 @@
 import { createHmac } from 'crypto';
 import { hashSync, compareSync } from 'bcryptjs';
+
 const secret = process.env.SECRET;
-export const gemHmac = (plainPassText: any, alg?: string): string => {
-    return createHmac(alg || 'sha256', secret).update(plainPassText).digest('hex');
+
+/**
+ * Generate Hmac from password.
+ * 
+ * @param plainPassText Receive plain text.
+ * @param alg Optional param, Default `sha256`, others options (sha1, sha512).
+ * @param digest Optional param, Default hex, base64.
+ * @returns `{string}` hashHmac.
+ */
+export const genHmac = (
+    plainPassText: any, alg?: string, digest?: string | any
+    ): string => {
+    return createHmac(alg || 'sha256', secret).update(plainPassText)
+        .digest(digest || 'hex');
 };
 
-export const plainTextHmacCompare = (plainPassword: string, hmac: string ): boolean => {
-    return gemHmac(plainPassword) === hmac;
+/**
+ * Compare plain text with before generated hmac.
+ * 
+ * @param plainPassword Receive plain password.
+ * @param hmac Receive before Hmac generated.
+ * @returns `{boolean}` Boolean.
+ */
+export const plainTextHmacCompare = (
+    plainPassword: string, hmac: string
+    ): boolean => {
+    return genHmac(plainPassword) === hmac;
 };
 
-export const gemBcryptHmac = (hmac: string, salt: number):string => {
+/**
+ * Generate bcrypt hash from hmac.
+ * 
+ * @param hmac Receive a Hmac.
+ * @param salt Prefer salt number, init 4.
+ * @returns `{string} Bcrypt Hash.
+ */
+export const hmacToBcrypt = (hmac: string, salt: number):string => {
     return hashSync(hmac, salt);
 };
 
-export const bcryptCompare = (plainPassword: string, hashBcypt: string): boolean => {
-    return compareSync(gemHmac( plainPassword ), hashBcypt);
+/**
+ * Compare bcrypt sign with plain password.
+ * 
+ * @param plainPassword Receive plain Text.
+ * @param hashBcypt Bcrypt before generated hash.
+ * @returns `{boolean}` Boolean.
+ */
+export const bcryptComparePassword = (
+    plainPassword: string, hashBcypt: string
+    ): boolean => {
+    return compareSync( genHmac( plainPassword ), hashBcypt );
 };
