2d669c1b0562a054ba87794fdfcd5a2d76938816
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-07-31 12:41:30 -0300

    [wip] all code content in this archive, is temp

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import UserRepository from '../database/repository/UserRepository';
-import { gemHmac, gemBcryptHmac, bcryptCompare} from '../utils/security';
-import jsonwebtoken  from 'jsonwebtoken';
+import { bcryptComparePassword } from '../utils/security';
+import { sign, verify } from 'jsonwebtoken';
 
 const secret = process.env.SECRET
 
@@ -10,20 +10,22 @@ const auth = {
         const {email , password} = req?.body;
         if (!email || !password) return res.json({});
         const user = await UserRepository.findByColumn({email});
-        const bindPassword = bcryptCompare(password, user[0].password);
+        const bindPassword = bcryptComparePassword(password, user[0].password);
         if (!bindPassword) return res.status(401).json({});
-        delete user[0].password;
+
+        const cleanUser = { ...user[0], email: "**********", password: "**********" }
         
         return res.status(200).json({
-            token: jsonwebtoken.sign({
-                data: user
+            data: cleanUser,
+            token: sign({
+                data: cleanUser
             }, secret, {expiresIn: '1h'})
         })
     },
     async tokenValidate(req: Request, res: Response, next: NextFunction) {
         try {
             const token = req.body.token;
-            const isValid = jsonwebtoken.verify(token, secret)
+            const isValid = verify(token, secret)
             
             if (isValid) return next();
         } catch(err) {
