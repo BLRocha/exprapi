097f2c2f38fa96838b29d8b16d5a1df0df1d9997
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2021-08-14 14:20:09 -0300

    move jwt custom methods for security

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,5 +1,6 @@
 import { createHmac } from 'crypto';
 import { hashSync, compareSync } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 const secret = process.env.SECRET;
 
@@ -53,4 +54,28 @@ export const bcryptComparePassword = (
     plainPassword: string, hashBcypt: string
     ): boolean => {
     return compareSync( genHmac( plainPassword ), hashBcypt );
+};
+
+/**
+ * 
+ * @param data Received type of data `{Object}` `{String}` `{number}`.
+ * @param expires Received a string Ex: `30m`, default `1h`.
+ * @returns Return token `{String}`
+ */
+export const makeToken = (data: any, expires?: string): string => {
+    return sign({ data }, secret, { expiresIn: expires || '1h' });
+};
+
+/**
+ * 
+ * @param token Received one token jwt
+ * @returns return a boolean
+ */
+export const checkToken = (token: string): boolean => {
+    try {
+        const isValid = verify(token, secret);
+        if (isValid) return true;
+    } catch(err) {
+        return false;
+    }
 };
